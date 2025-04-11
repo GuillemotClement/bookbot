43f1dfaef7de8f8ea9e3d717f5bc790cf86f303c
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-11 22:06:37 +0200

    Feat: show number of word in the file

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def get_book_text(filePath):
     file_contents = f.read()
   return file_contents
 
+def count_number_of_words(content):
+  list_word = content.split()
+  return len(list_word)
+
 def main():
-  print(get_book_text('./books/frankenstein.txt'))
+  content_book = get_book_text('./books/frankenstein.txt') 
+  num_words = count_number_of_words(content_book)
+  print(f"{num_words} words found in the document")
 
 main()
