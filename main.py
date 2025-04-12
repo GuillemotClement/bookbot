d36e781cb44f75bf50bdf25b566f7bb3d23175f9
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-12 09:08:31 +0200

    Feat: add function for counting characters

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # importations des fonctions du fichier stats
 # stats est le nom du fichier contenant les fonctions.
-from stats import get_book_text, count_number_of_words
+from stats import get_book_text, count_number_of_words, count_cars
 
 
 def main():
-  content_book = get_book_text('./books/frankenstein.txt') 
+  book = "./books/frankenstein.txt"
+  content_book = get_book_text(book) 
   num_words = count_number_of_words(content_book)
-  print(f"{num_words} words found in the document")
-
+  dict_caract = count_cars(content_book, num_words)
+  print(dict_caract)
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,14 @@ def get_book_text(filePath):
 def count_number_of_words(content):
   list_word = content.split()
   return len(list_word)
+
+def count_cars(content, num_of_cars):
+  cars = {}
+  for key in content:
+    lower_caractere = key.lower()
+    if lower_caractere in cars:
+      cars[lower_caractere] += 1
+    else:
+      cars[lower_caractere] = 1
+
+  return cars
