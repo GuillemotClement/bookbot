94175fc64429447773154843d7f3c0b832b2acf8
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-12 09:44:00 +0200

    Feat: add rapport with triage

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # importations des fonctions du fichier stats
 # stats est le nom du fichier contenant les fonctions.
-from stats import get_book_text, count_number_of_words, count_cars
+from stats import get_book_text, count_number_of_words, count_cars, get_reports
 
 
 def main():
   book = "./books/frankenstein.txt"
   content_book = get_book_text(book) 
   num_words = count_number_of_words(content_book)
-  dict_caract = count_cars(content_book, num_words)
-  print(dict_caract)
+  dict_caract = count_cars(content_book)
+  get_reports(dict_caract, num_words, book)
+
+
 main()
--- stats.py
+++ stats.py
@@ -7,13 +7,28 @@ def count_number_of_words(content):
   list_word = content.split()
   return len(list_word)
 
-def count_cars(content, num_of_cars):
+def count_cars(content):
   cars = {}
   for key in content:
     lower_caractere = key.lower()
-    if lower_caractere in cars:
-      cars[lower_caractere] += 1
-    else:
-      cars[lower_caractere] = 1
-
+    if lower_caractere.isalpha():
+      if lower_caractere in cars:
+        cars[lower_caractere] += 1
+      else:
+        cars[lower_caractere] = 1
   return cars
+
+def get_reports(count_rapport, count_of_char, path_to_book):
+  # triage du rapport du plus grand nombre d'occurence au plus petit
+  # count_rapport.items() -> convertis le dictionnaire en tuple
+  # key=lambda .. creer une fonction anonyme pour le triage
+  sorted_item = sorted(count_rapport.items(), key=lambda item: item[1], reverse=True)
+
+  print("============= BOOKBOT =============")
+  print(f"Analyzing book found at {path_to_book}")
+  print("------------- Word Count ----------")
+  print(f"Found {count_of_char} total words")
+  print("-------------- Character Count ----")
+  for char, count in sorted_item:
+    print(f"{char}: {count}")
+  print("============= END =================")
