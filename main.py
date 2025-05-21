e9b5aac9acc1473b2674841bda286ff1a23e3ea4
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-21 07:49:55 +0200

    read from file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_path = "./books/frankenstein.txt"
+    data = get_book_text(book_path)
+    print(data)
+
+main()
