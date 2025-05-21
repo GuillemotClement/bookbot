a1f1c7467d456f2c788d25da4848eed963216167
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-21 07:56:22 +0200

    add count words

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ def get_book_text(filepath):
         # retourne le contenu du fichier
         return f.read()
 
+def get_number_of_words(data):
+    # conversion en split
+    list_words = data.split()
+    # retourne le nombre de mots dans le texte
+    return len(list_words)
 
 def main():
     # definition du path vers le document
     book_path = "./books/frankenstein.txt"
     # recuperation du content
     text = get_book_text(book_path)
-    # affichage du content
-    print(text)
+    # appelle de la fonction pour retourner le nombre de mots du texte
+    count_words = get_number_of_words(text)
+
+    print(f"{count_words} words found in the document")
 
 main()
